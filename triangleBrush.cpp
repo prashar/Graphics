02634a29ef40a4026e47c3304ec4730f9b1283ad
prashar <neeraj.prashar@gmail.com>
2014-10-05 17:34:47 -0700

    Adding the triangle brush implementation

---
+++ triangleBrush.cpp
@@ -0,0 +1,63 @@
+//
+// TriangleBrush.cpp
+//
+// The implementation of Point Brush. It is a kind of ImpBrush. All your brush implementations
+// will look like the file with the different GL primitive calls.
+//
+
+#include "impressionistDoc.h"
+#include "impressionistUI.h"
+#include "TriangleBrush.h"
+
+extern float frand();
+
+TriangleBrush::TriangleBrush( ImpressionistDoc* pDoc, char* name ) :
+	ImpBrush(pDoc,name)
+{
+}
+
+void TriangleBrush::BrushBegin( const ImpBrush::Point source, const ImpBrush::Point target )
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg=pDoc->m_pUI;
+
+	int size = pDoc->getSize();
+
+
+
+	glPointSize( (float)size );
+
+	BrushMove( source, target );
+}
+
+void TriangleBrush::BrushMove( const ImpBrush::Point source, const ImpBrush::Point target )
+{
+	ImpressionistDoc* pDoc = GetDocument();
+	ImpressionistUI* dlg=pDoc->m_pUI;
+	int size = pDoc->getSize(); 
+	int Ax, Ay, Bx, By, Cx, Cy; 
+	Ax = target.x - (.5*size); 
+	Bx = target.x + (.5*size);
+	Cx = target.x;
+	Ay = target.y - (.5*size);
+	By = target.y - (.5*size);
+	Cy = target.y + (.5*size);
+
+	if ( pDoc == NULL ) {
+		printf( "TriangleBrush::BrushMove  document is NULL\n" );
+		return;
+	}
+
+	glBegin( GL_POLYGON );
+		SetColor( source );
+		glVertex2i(Ax, Ay);
+		glVertex2i(Bx, By);
+		glVertex2i(Cx, Cy);
+	glEnd();
+}
+
+void TriangleBrush::BrushEnd( const ImpBrush::Point source, const ImpBrush::Point target )
+{
+	// do nothing so far
+}
+
